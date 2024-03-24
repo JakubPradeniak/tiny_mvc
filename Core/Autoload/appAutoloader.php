7892fa3238a65ddb5a25f20972d00e093dfc8722
Jakub Pradeniak <jakub.pradeniak@bluenet.cz>
2024-03-24 17:27:12 +0100

    Fix app src autoloader

--- Core/Autoload/appAutoloader.php
+++ Core/Autoload/appAutoloader.php
@@ -7,7 +7,7 @@ spl_autoload_register(function ($class) {
     $prefix = 'App';
 
     // base directory for the namespace prefix
-    $base_dir = str_replace(['\Autoload', '/Autoload'], '', __DIR__);
+    $base_dir = str_replace(['\Core\Autoload', '/Core/Autoload'], '', __DIR__);
 
     // does the class use the namespace prefix?
     $len = strlen($prefix);
@@ -22,6 +22,9 @@ spl_autoload_register(function ($class) {
     // separators with directory separators in the relative class name, append
     // with .php
     $file = $base_dir . '/src' . str_replace('\\', '/', $relative_class) . '.php';
+
+    var_dump($base_dir);
+    var_dump($file);
 
     // if the file exists, require it
     if (file_exists($file)) {
