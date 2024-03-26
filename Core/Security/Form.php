136d70f89d367ea7ab7863e8fd3a5108b9da6314
Jakub Pradeniak <jakub.pradeniak@bluenet.cz>
2024-03-26 13:25:26 +0100

    Create csrf token for forms

---
+++ Core/Security/Form.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Security;
+
+use Core\Persist\Session;
+
+class Form
+{
+    const TOKEN_ID = 'csrf_token';
+
+    public static function getToken(): string
+    {
+        $token = hash('sha512', microtime());
+
+        Session::set('csrf_token', $token);
+
+        return '<input type="hidden" name="' . self::TOKEN_ID . '" value="' . $token . '">';
+    }
+
+    public static function validateToken(string $token): bool
+    {
+        $storedToken = Session::get(self::TOKEN_ID);
+
+        return $token === $storedToken;
+    }
+}
